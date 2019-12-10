189482c1e6d3b53f7221d8f5106def2eb91f413f
Jason Baker <jbaker@freightwaves.com>
2019-12-09 22:23:13 -0500

    Various updates

--- src/ogrupload.sh
+++ src/ogrupload.sh
@@ -3,5 +3,5 @@ files=(osmfiles/*.osm)
 
 for file in $files 
 do
-ogr2ogr -append -f PostgreSQL PG:"user=$fwpguser password=$fwpgpass host=$fwpghost dbname=FWGeos" $file lines -nln "osm_ways" -lco "FID=id" -lco "GEOMETRY_NAME=geom" -nlt PROMOTE_TO_MULTI
+ogr2ogr -append -f PostgreSQL PG:"user=$fwpguser password=$fwpgpass host=$fwpghost dbname=FWGeos" $file lines -nln "osm_ways" -lco "FID=id" -lco "GEOMETRY_NAME=geom"
 done
